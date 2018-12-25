f267867528f241cc2b932eedb9327639da98fceb
unknown <okunade.stephenmuyiwa@gmail.com>
2018-12-25 11:45:01 +0100

    Updated pages and routes

--- AngularUI/src/app/app.component.spec.ts
+++ AngularUI/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'appr'`, () => {
+  it(`should have as title 'apppp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('appr');
+    expect(app.title).toEqual('apppp');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to appr!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to apppp!');
   });
 });
--- AngularUI/src/app/components/settings/settings.component.ts
+++ AngularUI/src/app/components/settings/settings.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.scss']
+  styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
 
