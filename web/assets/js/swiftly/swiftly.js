77eb1eceea501fc7219453bee05462c49ebd7d55
Ogunfemi Micheal <mikebellami7@gmail.com>
2019-02-05 15:33:13 +0100

    transfer page completed

--- web/assets/js/swiftly/swiftly.js
+++ web/assets/js/swiftly/swiftly.js
@@ -26,4 +26,21 @@ $(document).ready(()=>{
     })
 
     $('.right-slidebar').each('div', new SimpleBar);
+
+    $(".next_btn").click(function() { // Function Runs On NEXT Button Click
+        $(this).parent().next().fadeIn('slow');
+        $(this).parent().css({
+        'display': 'none'
+        });
+        // Adding Class Active To Show Steps Forward;
+        $('.active').next().addClass('active');
+        });
+        $(".pre_btn").click(function() { // Function Runs On PREVIOUS Button Click
+        $(this).parent().prev().fadeIn('slow');
+        $(this).parent().css({
+        'display': 'none'
+        });
+        // Removing Class Active To Show Steps Backward;
+        $('.active:last').removeClass('active');
+    });
 })
